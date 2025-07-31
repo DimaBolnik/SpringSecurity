b557e6329b3d27d93bca41b13698e8d016cb3fc2
Dima Bolnik <dimabolnik@mail.ru>
2025-07-31 18:33:43 +0300

    создание клиента для ферификации + странички html

---
+++ js-oauth2-frontend/src/main/java/ru/bolnik/jsoauth2frontend/controller/RedirectController.java
@@ -0,0 +1,25 @@
+package ru.bolnik.jsoauth2frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RedirectController {
+
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/redirect")
+    public String redirect() {
+        return "redirect";
+    }
+
+    @PostMapping("/result")
+    public String result() {
+        return "index";
+    }
+}
