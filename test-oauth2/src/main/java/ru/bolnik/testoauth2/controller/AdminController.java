e3ffd102e7cc733222fe7ca034b7bad0477ecb08
Dima Bolnik <dimabolnik@mail.ru>
2025-08-06 18:13:00 +0300

    Рефактор

--- test-oauth2/src/main/java/ru/bolnik/testoauth2/controller/AdminController.java
+++ test-oauth2/src/main/java/ru/bolnik/testoauth2/controller/AdminController.java
@@ -1,6 +1,5 @@
 package ru.bolnik.testoauth2.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,28 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/test")
+@RequestMapping("/admin")
 @RestController
-public class TestController {
+public class AdminController {
 
     @GetMapping("/delete/{id}")
-    @PreAuthorize("hasRole('admin')")
-    public String delete(@PathVariable String id, @AuthenticationPrincipal Jwt jwt)
-    {
+    public String delete(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         System.out.println("jwt.getClaim(\"email\") = " + jwt.getClaim("email"));
         System.out.println("id delete = " + id);
         return "delete work";
     }
 
     @GetMapping("/add")
-    @PreAuthorize("hasRole('admin')")
-    public String add(){
+    public String add() {
         return "add work";
     }
 
-    @GetMapping("/view")
-    @PreAuthorize("hasRole('user')")
-    public String view(){
-        return "view work";
-    }
 }
