afe43dc7b15ec434094279192aa9865a2351d7b1
Dima Bolnik <dimabolnik@mail.ru>
2025-08-06 16:33:31 +0300

    добавление конвертера JWT и его внедрение

--- test-oauth2/src/main/java/ru/bolnik/testoauth2/controller/TestController.java
+++ test-oauth2/src/main/java/ru/bolnik/testoauth2/controller/TestController.java
@@ -1,6 +1,10 @@
 package ru.bolnik.testoauth2.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/login")
-    public String login(){
-        return "login work";
+    @GetMapping("/delete/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public String delete(@PathVariable String id, @AuthenticationPrincipal Jwt jwt)
+    {
+        System.out.println("jwt.getClaim(\"email\") = " + jwt.getClaim("email"));
+        System.out.println("id delete = " + id);
+        return "delete work";
     }
 
-    @GetMapping
-    public String internal(){
-        return "internal work";
+    @GetMapping("/add")
+    @PreAuthorize("hasRole('admin')")
+    public String add(){
+        return "add work";
+    }
+
+    @GetMapping("/view")
+    @PreAuthorize("hasRole('user')")
+    public String view(){
+        return "view work";
     }
 }
-
