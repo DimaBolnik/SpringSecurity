efb7c1972eca2c8b3c8c562e31e16a6f5437f2a9
Dima Bolnik <dimabolnik@mail.ru>
2025-08-07 10:33:30 +0300

    удаление лишнего кода 2

--- js-oauth2-bff/src/main/java/ru/bolnik/jsoauth2bff/JsOauth2BffApplication.java
+++ js-oauth2-bff/src/main/java/ru/bolnik/jsoauth2bff/JsOauth2BffApplication.java
@@ -1,13 +1,13 @@
-package ru.bolnik.fooddelivery;
+package ru.bolnik.jsoauth2bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class JsOauth2BffApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(JsOauth2BffApplication.class, args);
     }
 
 }
